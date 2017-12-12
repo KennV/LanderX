08bd9d275e8a121e20285614aa7e98f2a9e6c9f4
Kenn Villegas <kenn.@phoenix.64.local>
2017-12-12 18:15:20 -0500

    INITIAL APPLICATION STATE
    Architecture, State, and Structure begin here

---
+++ LanderX/LanderX/ViewControllers/KDVMapViewController/KDVMapDetailViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.h
+//  LanderX
+//
+//  Created by Kenn Villegas on 12/12/17.
+//  Copyright © 2017 dubian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LanderX+CoreDataModel.h"
+
+@interface KDVMapDetailViewController : UIViewController
+
+@property (strong, nonatomic) Event *detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ LanderX/LanderX/ViewControllers/KDVMapViewController/KDVMapDetailViewController.m
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.m
+//  LanderX
+//
+//  Created by Kenn Villegas on 12/12/17.
+//  Copyright © 2017 dubian. All rights reserved.
+//
+
+//#import "KDVMapDetailViewController.h"
+#import "KDVMapDetailViewController.h"
+
+
+@interface KDVMapDetailViewController ()
+
+@end
+
+@implementation KDVMapDetailViewController
+
+- (void)configureView {
+  // Update the user interface for the detail item.
+  if (self.detailItem) {
+      self.detailDescriptionLabel.text = self.detailItem.timestamp.description;
+  }
+}
+
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+  [self configureView];
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(Event *)newDetailItem {
+  if (_detailItem != newDetailItem) {
+      _detailItem = newDetailItem;
+      
+      // Update the view.
+      [self configureView];
+  }
+}
+
+
+@end
