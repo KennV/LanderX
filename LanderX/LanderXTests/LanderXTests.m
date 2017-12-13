f7fd99d9f7c06f9521e97c3621736fd1d636980b
Kenn Villegas <kenn.@phoenix.64.local>
2017-12-13 18:31:41 -0500

    TESTS MODIFIED
    I suppose that I should really make a test for the ViewControllers, and
    have that be a very separate test from the DataModel

--- LanderX/LanderXTests/LanderXTests.m
+++ LanderX/LanderXTests/LanderXTests.m
@@ -8,22 +8,25 @@
 */
 
 #import <XCTest/XCTest.h>
+/* Note: You Must include the model in the compiler's test target */
 #import <CoreData/CoreData.h>
-/*Note: You Must include the model in the test compile*/
 #import "KDVPrimeTVController.h"
 
-
 @interface LanderXTests : XCTestCase
+
 @property (strong, nonatomic)NSManagedObjectContext *testMOC;
 @property (strong, nonatomic)KDVPrimeTVController *SUT;
-
+@property (strong, nonatomic)NSPersistentStoreCoordinator *PSK;
 @end
 
 @implementation LanderXTests
 @synthesize testMOC = _testMOC;
 @synthesize SUT = _SUT;
+@synthesize PSK = _PSK;
+
 /**
- OKay, these; the first set of tests should only determine if the Data {MOC, MOM, and PSK } are healthy
+
+OKay, these; the first set of tests should only determine if the Data {MOC, MOM, and PSK } are healthy
 _Alternately_ I can also write failing tests and see what the causes and features of these failures are
 Regardless I need to set up an in memory MOC which is actually just longhand for PSK.viewContext
  
@@ -34,28 +37,42 @@ return the new
 -elseIf return old
 */
 
+- (void)setupInMemoryPSK {
+  //
+// NSManagedObjectModel *tMOM = [NSManagedObjectModel mergedModelFromBundles:[NSBundle main]];
+//  NSManagedObjectModel *tMOM = [NSManagedObjectModel mergedModelFromBundles:(NSBundle.mainBundle)];
+//  NSPersistentStoreCoordinator *tPOP = [[NSPersistentStoreCoordinator alloc]initWithManagedObjectModel:tMOM];
+  
+}
+
 - (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+  [super setUp];
+  // Put setup code here. This method is called before the invocation of each test method in the class.
   [self setSUT:[[KDVPrimeTVController alloc]init]];
+//  [self setupInMemoryPSK];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
   [self setSUT:nil];
-    [super tearDown];
+  [super tearDown];
 }
 
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
-/* OkaY I can easily test that it exists, but what does it relly look lik ein runtime
-it is just a breakpoint away b\c I _feel_ I should be able to see the MOC 
+/* Okay I can easily test that it exists, but what does it relly look like in runtime
+it is just a breakpoint away b\c I _feel_ I should be able to see the MOC (At least the current / demo PSK)
 */
 - (void)testSUTExists {
   XCTAssertNotNil(_SUT);
+  XCTAssertNotNil([self SUT]);
+  // these two fail because it is only init'd up there the view
+  // the data has not been loaded
+//  [[self SUT] viewDidLoad];
 //  XCTAssertNotNil([[self SUT]managedObjectContext]);
+  
 }
 
 - (void)testPerformanceExample {
