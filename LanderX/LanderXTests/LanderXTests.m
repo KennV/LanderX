1cd2889c9159de00f4c15d86f2002d62413ba45e
Kenn Villegas <kenn.@phoenix.64.local>
2017-12-12 21:12:08 -0500

    STUBBED UP THE TEST SUITE
    With a little bit of snazzy documentation
    the goal is to maximize coverage

--- LanderX/LanderXTests/LanderXTests.m
+++ LanderX/LanderXTests/LanderXTests.m
@@ -1,18 +1,38 @@
-//
-//  LanderXTests.m
-//  LanderXTests
-//
-//  Created by Kenn Villegas on 12/12/17.
-//  Copyright © 2017 dubian. All rights reserved.
-//
+/**
+  LanderXTests.m
+  LanderXTests
+
+  Created by Kenn Villegas on 12/12/17.
+  Copyright © 2017 dubian. All rights reserved.
+
+*/
 
 #import <XCTest/XCTest.h>
+#import <CoreData/CoreData.h>
+/*Note: You Must include the model in the test compile*/
+#import "KDVPrimeTVController.h"
+
 
 @interface LanderXTests : XCTestCase
+@property (strong, nonatomic)NSManagedObjectContext *testMOC;
+@property (strong, nonatomic)KDVPrimeTVController *SUT;
 
 @end
 
 @implementation LanderXTests
+@synthesize testMOC = _testMOC;
+@synthesize SUT = _SUT;
+/**
+ OKay, these; the first set of tests should only determine if the Data {MOC, MOM, and PSK } are healthy
+_Alternately_ I can also write failing tests and see what the causes and features of these failures are
+Regardless I need to set up an in memory MOC which is actually just longhand for PSK.viewContext
+ 
+If the *new !== old
+retain the new
+release the old
+return the new
+-elseIf return old
+*/
 
 - (void)setUp {
     [super setUp];
@@ -27,6 +47,7 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+  
 }
 
 - (void)testPerformanceExample {
