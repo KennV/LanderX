ffa9170542d37828829a19abc07c7a727440fcdb
Kenn Villegas <kenn.@phoenix.64.local>
2017-12-19 17:06:51 -0500

    ADDED APP_DATACONTOLLER AND TESTS
    KDVApplicationDataController and KDVApplicationDataControllerTests

---
+++ LanderX/LanderX/Model/KDVAbstractControllers/KDVApplicationDataController.m
@@ -0,0 +1,14 @@
+/**
+  KDVApplicationDataController.m
+  LanderX
+
+  Created by Kenn Villegas on 12/19/17.
+  Copyright © 2017 dubian. All rights reserved.
+
+*/
+
+#import "KDVApplicationDataController.h"
+
+@implementation KDVApplicationDataController
+
+@end
