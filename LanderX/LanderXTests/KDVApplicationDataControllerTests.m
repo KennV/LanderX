fc8cd8243bd740f78bcccf51e5ec1dcbb1c91c03
Kenn Villegas <kenn.@phoenix.64.local>
2017-12-19 22:29:09 -0500

    STARTING WITH SIMPLE TESTS
    SRSLY isKind/isMember
    PSK != nil

--- LanderX/LanderXTests/KDVApplicationDataControllerTests.m
+++ LanderX/LanderXTests/KDVApplicationDataControllerTests.m
@@ -7,20 +7,19 @@
 
 */
 
-#import <XCTest/XCTest.h>
-
-#import <CoreData/CoreData.h>
-#import "KDVApplicationDataController.h"
-
 /**
 Ok here are some proposed tests
-- a different PSK as a _lazy init
-- a thorough breakdown of the init-chain
-- Object-Class
+- a thorough breakdown of the init-chain {high}
+ - do I init well
+ - does my Object-Class init well
+- Object-Class {high}
  - object sub-entities
  - KDVApplicationDataController
  - setting the subEntity from here?
  - testing all values in the SubEntities
+- a different PSK as a _lazy init {medium}
+- Load / Save / Delete {defer}
+- currentEntity() {optional}
 - Possibly Controller Protocol?
  - might not be needed in this class but used in
  - SomeVueController <ApplicationDataProtocol>
@@ -28,15 +27,51 @@ Ok here are some proposed tests
 - currentEntity()
 */
 
+#import <XCTest/XCTest.h>
+
+#import <CoreData/CoreData.h>
+#import "KDVApplicationDataController.h"
+
+
 @interface KDVApplicationDataControllerTests : XCTestCase
+
+@property (strong, nonatomic)KDVApplicationDataController *SUT;
+@property (strong, nonatomic)NSPersistentContainer *PSK;
 
 @end
 
 @implementation KDVApplicationDataControllerTests
 
+- (NSPersistentContainer *)PSK {
+  // The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
+  @synchronized (self) {
+    //    NSError *error = nil;
+    if (_PSK == nil) {
+      // _THIS IS THE ONLY REAL DIFFERENCE BEWEEN PRODUCTION() AND TEST()
+      NSPersistentStoreDescription *dsc = [[NSPersistentStoreDescription alloc]init];
+      [dsc setType:(@"NSInMemoryStoreType")]; //dsc.type = (NSInMemoryStoreType);
+      
+      _PSK = [[NSPersistentContainer alloc] initWithName:@"LanderX"];
+      [_PSK loadPersistentStoresWithCompletionHandler:^(NSPersistentStoreDescription *dsc, NSError *error)
+       
+       {
+         if (error != nil) {
+           
+           NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+           abort();
+         }
+       }];
+    }
+  }
+  
+  return _PSK;
+}
+
+
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+  [self setSUT:[[KDVApplicationDataController alloc]initAllDefaults]];
 }
 
 - (void)tearDown {
@@ -44,16 +79,12 @@ Ok here are some proposed tests
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testOne {
+  XCTAssertNotNil([self SUT]);
+  XCTAssert([[self SUT]isMemberOfClass:[KDVApplicationDataController class]]);
+  XCTAssert([[self SUT]isKindOfClass:[KDVAbstractDataController class]]);
 }
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testTwo {
+  XCTAssertNotNil([self PSK]);
 }
-
 @end
